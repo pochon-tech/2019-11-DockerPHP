72382ab3c5296787ae7a91fb99f2203c4710db0f
pochon-tech <pochon.tech.0519@gmail.com>
2019-11-28 03:03:03 +0900

    Php & Mysql

--- v1/data/html/index.php
+++ v1/data/html/index.php
@@ -1,16 +1,33 @@
-<?php
-phpinfo();
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>nginx Static Server</title>
+    <title>nginx + PHP + MySQL</title>
 </head>
 <body>
     <div class="container">
-        <h1 class="title">nginx Static Server</h1>
+        <h1 class="title">nginx + PHP + MySQL</h1>
+
+        <?php
+        $mysql = new mysqli($_ENV['DATABASE_HOST'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD'], $_ENV['MYSQL_DATABASE']);
+        if (!$mysql) {
+            echo "Error: Unable to connect to MySQL." . PHP_EOL;
+            echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+            exit;
+        }
+        $sql = "INSERT INTO hoges(created_at) VALUES('" . date('Y-m-d H:i:s') . "')";
+        $result = $mysql->query($sql);
+        $sql = "SELECT * FROM hoges ORDER BY hoge_id desc limit 1";
+        $result = $mysql->query($sql)->fetch_row();
+        echo '<pre class="log">';
+        var_dump($result);
+        echo '</pre>';
+        mysqli_close($mysql);
+        phpinfo();
+        ?>
     </div>
+    <script>
+        console.log(`this. page is ${ document.title }`)
+    </script>
 </body>
 </html>
